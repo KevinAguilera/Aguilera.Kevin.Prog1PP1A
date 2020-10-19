d69ec9c4d2c35a7ae4c1fb1a00fb0dc834c6e778
KevinAguilera <67611246+KevinAguilera@users.noreply.github.com>
2020-10-19 14:11:11 +0100

    Update main.c

--- Ejercicios/main.c
+++ Ejercicios/main.c
@@ -1,46 +1,129 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#define LEN_NOTEBOOK 3
 
-float aplicarDescuento(float precioProducto);
-int contarCaracteres(char string[],char caracter);
+typedef struct
+{
+    int id;
+    char procesador[20];
+    char marca[20];
+    float precio;
+} eNotebook;
+
+void aplicarDescuento(float precio, float* resultado);
+int contarCaracteres(char* cadena, char caracter);
+int sortNotebooks(eNotebook* list, int len);
+void showNotebook(eNotebook computadora);
+int printNotebooks(eNotebook* list, int len);
 
 
 int main()
 {
-    float precio=100;
+    float precio = 100;
+    float resultadoConDescuento;
+    aplicarDescuento(precio, &resultadoConDescuento);
+    printf("El precio es: %.2f\n", precio);
+    printf("El precio con descuento es: %.2f\n", resultadoConDescuento);
+    printf("\n\n");
 
-    precio=aplicarDescuento(precio);
+    char frase[]="Hola como estas";
+    printf("%s\n", frase);
+    printf("La frase tiene %d cantidad de o\n", contarCaracteres(frase, 'o'));
+    printf("\n\n");
 
-    printf("El precio con descuento es: $%.2f\n\n",precio);
+    eNotebook notebooks[LEN_NOTEBOOK] =
+    {
+        {1000, "I5", "INTEL", 25000.75},
+        {1000, "I9", "INTEL", 40000.75},
+        {1001, "I9", "IBM", 80000.89},
+    };
 
-    char palabra[50]="calavera",caracter='a';
-    int cant;
-
-    cant=contarCaracteres(palabra,caracter);
-    printf("El caracter %c aparece %d veces en la palabra %s.\n",caracter,cant,palabra);
-    return 0;
-
+    printNotebooks(notebooks, LEN_NOTEBOOK);
+    sortNotebooks(notebooks, LEN_NOTEBOOK);
+    printNotebooks(notebooks, LEN_NOTEBOOK);
 
     return 0;
 }
 
-float aplicarDescuento(float precioProducto)
+void aplicarDescuento(float precio, float* resultado)
 {
-    float retorno;
-    retorno =precioProducto-(precioProducto*0.05);
-    return retorno;
+    float porcentaje;
+    porcentaje = (precio * 5) / 100;
+    *resultado = precio - porcentaje;
 }
 
-int contarCaracteres(char string[],char caracter)
+int contarCaracteres(char* cadena, char caracter)
 {
-    int contador=0;
-
-    for(int i = 0; string[i]!= '\0' ;i++)
-   {
-       if(string[i]==caracter)
+    int lenCadena = strlen(cadena);
+    int contarCaracter = 0;
+    for(int i = 0; i < lenCadena; i++)
+    {
+        if(cadena[i] == caracter)
         {
-            contador++;
+            contarCaracter++;
         }
-   }
-   return contador;
+    }
+    return contarCaracter;
+}
+
+int sortNotebooks(eNotebook* list, int len)
+{
+    int error = -1;
+    eNotebook auxNotebook;
+
+    if(list!= NULL && len > 0)
+    {
+        for(int i = 0; i < len-1; i++)
+        {
+            for(int j = i + 1; j < len; j++)
+            {
+                if(strcmp(list[i].marca, list[j].marca) > 0)
+                {
+                    auxNotebook = list[i];
+                    list[i] = list[j];
+                    list[j] = auxNotebook;
+                }
+                else if(strcmp(list[i].marca, list[j].marca) == 0 && list[i].precio < list[j].precio)
+                {
+                    auxNotebook = list[i];
+                    list[i] = list[j];
+                    list[j] = auxNotebook;
+                }
+            }
+        }
+        error = 0;
+
+    }
+
+    return error;
+}
+
+
+int printNotebooks(eNotebook* list, int len)
+{
+    if(list != NULL && len > 0 && len <= 10)
+    {
+        printf("******************************************************************\n");
+        printf("  ID            PROCESADOR           MARCA              PRECIO\n");
+        printf("******************************************************************\n");
+        for(int i = 0; i < len; i++)
+        {
+                showNotebook(list[i]);
+        }
+        printf("\n\n");
+        return 0;
+    }
+    return -1;
+}
+
+void showNotebook(eNotebook computadora)
+{
+    printf("%6d %15s            %8s              $%4.2f\n",
+           computadora.id,
+           computadora.procesador,
+           computadora.marca,
+           computadora.precio
+          );
 }
